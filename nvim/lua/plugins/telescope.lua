57dbefe20ba202ba7b15757b57f69fe45be3a0e4
samcx <sam@vercel.com>
2024-03-18 00:03:11 -0700

    Update telescope.lua

--- nvim/lua/plugins/telescope.lua
+++ nvim/lua/plugins/telescope.lua
@@ -1,13 +1,16 @@
 local keymap = vim.keymap
 
 return {
-  "nvim-telescope/telescope.nvim",
-  branch = "0.1.x",
-  dependencies = { "nvim-lua/plenary.nvim" },
-  config = function()
-    local builtin = require("telescope.builtin")
+  {
+    -- Find, Filter, Preview, Pick. All Lua, all the time.
+    "nvim-telescope/telescope.nvim",
+    branch = "0.1.x",
+    dependencies = { "nvim-lua/plenary.nvim" },
+    config = function()
+      local builtin = require("telescope.builtin")
 
-    keymap.set("n", "<C-p>", builtin.find_files, {})
-    keymap.set("n", "<leader>fg", builtin.live_grep, {})
-  end,
+      keymap.set("n", "<C-p>", builtin.find_files, { desc = "[S]earch [F]iles" })
+      keymap.set("n", "<leader>sg", builtin.live_grep, { desc = "[S]earch by [G]rep" })
+    end,
+  }
 }
