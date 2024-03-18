2c907b90ae0f8c477e25b291067f7a85ba37524e
samcx <sam@vercel.com>
2024-03-17 21:55:13 -0700

    Update symlink.zsh to sync-dotfiles.zsh

--- scripts/sync-dotfiles.zsh
+++ scripts/sync-dotfiles.zsh
@@ -4,17 +4,11 @@ NVIM=$HOME/.config/nvim
 TARGET=$HOME/Documents/Igloo/dotfiles
 
 function relink() {
-  if [[ -f $2 ]]; then
-    rm $2
-  else echo $2 "doesn't exist!"
-  fi
-
-  ln -s $1 $2
-  echo "✓ Linked $2 to $1."
+  cp $1 $2
 }
 
 relink $NVIM/init.lua $TARGET/nvim/init.lua
 relink $NVIM/plugins.lua $TARGET/nvim/plugins.lua
 relink $NVIM/set.lua $TARGET/nvim/set.lua
 
-echo "Symlink complete."
+echo "✓ Symlink complete."
