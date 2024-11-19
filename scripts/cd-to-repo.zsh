90895df6de03708c0c0b3a3b86efd714cebea8cb
samcx <sam@vercel.com>
2024-11-19 13:02:40 -0800

    Add cd-to-repo script

---
+++ scripts/cd-to-repo.zsh
@@ -0,0 +1,22 @@
+#!/bin/zsh
+
+# A script for cd'ing into a commonly-used directories.
+
+if [[ $# != 1 ]]; then
+  echo "Missing correct number of arguments (1)."
+  return 1
+fi
+
+if [[ $1 = "front" ]]; then
+  cd ~/Documents/Vercel/front
+elif [[ $1 = "next" ]]; then
+  cd ~/Documents/Vercel/next.js
+elif [[ $1 = "samcx" ]]; then
+  cd ~/Documents/Igloo/samcx
+elif [[ $1 = "v0" ]]; then
+  cd ~/Documents/Vercel/v0
+else
+  echo "Directory not listed in script!"
+fi
+
+return 0
