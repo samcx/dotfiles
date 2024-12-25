0740bf4fc7ad85f6e879f7eee6fa6095e34a8ea9
samcx <sam@vercel.com>
2024-12-25 13:36:48 -0800

    Add /zed

--- scripts/sync-files.zsh
+++ scripts/sync-files.zsh
@@ -1,8 +1,13 @@
 #!/bin/zsh
 
-NVIM=$HOME/.config/nvim
 DOT=$HOME/Documents/Igloo/dotfiles
+NVIM=$HOME/.config/nvim
+ZED=$HOME/Documents/Igloo/dotfiles/zed
+ZED_KEYMAP=$HOME/.config/zed/keymap.json
+ZED_SETTINGS=$HOME/.config/zed/settings.json
 
 cp -r $NVIM $DOT
+cp -r $ZED_KEYMAP $ZED
+cp -r $ZED_SETTINGS $ZED
 
 echo "✓ Completed syncing files."
