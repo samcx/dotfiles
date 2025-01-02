ea00af0ea66f2cb77f74472bd18ac0fd43f8f36e
samcx <sam@vercel.com>
2025-01-02 12:42:00 -0800

    Add /ghostty

--- scripts/sync-files.zsh
+++ scripts/sync-files.zsh
@@ -1,11 +1,13 @@
 #!/bin/zsh
 
 DOT=$HOME/Documents/Igloo/dotfiles
+GHOSTTY=$HOME/.config/ghostty
 NVIM=$HOME/.config/nvim
 ZED=$HOME/Documents/Igloo/dotfiles/zed
 ZED_KEYMAP=$HOME/.config/zed/keymap.json
 ZED_SETTINGS=$HOME/.config/zed/settings.json
 
+cp -r $GHOSTTY $DOT
 cp -r $NVIM $DOT
 cp -r $ZED_KEYMAP $ZED
 cp -r $ZED_SETTINGS $ZED
